749218893a409fe444c4590b46c0c2618291aa17
YRGHALI <yrghali@alumno.upv.es@ldsic-vdi04.upvnet.upv.es>
2025-12-29 14:16:54 +0100

    Añadida implementación de la clase Node

---
+++ Node.h
@@ -0,0 +1,20 @@
+#ifndef NODE_H
+#define NODE_H
+#include <ostream>
+
+template <typename T>
+class Node {
+public:
+    T data;
+    Node<T>* next;
+
+    Node(T data, Node<T>* next = nullptr)
+        : data(data), next(next) {}
+    friend std::ostream& operator<<(std::ostream& out, const Node<T>& node) {
+        out << node.data;
+        return out;
+    }
+};
+
+#endif
+
