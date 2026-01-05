8305331f3d4e6d42e9ef1027cb71d44d956aad71
Yanis <yrghali@upv.edu.es>
2026-01-05 15:36:41 +0100

    Add virtual destructor to List class
    Add a virtual destructor to the List class.

--- List.h
+++ List.h
@@ -4,6 +4,8 @@
 template <typename T> 
 class List {
     public:
+         virtual ~List(){
+		 }
         virtual void insert(int pos, T e) = 0;
 	virtual void append(T e) = 0;
 	virtual void prepend(T e) = 0;
